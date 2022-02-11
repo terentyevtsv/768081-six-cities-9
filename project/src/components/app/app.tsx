f9e02cfdb9deacb55d007c08a5696c4d469f9131
TerentevSV <terentyevtsv@ya.ru>
2022-02-11 17:16:43 +0400

    Добавил основные компоненты

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,15 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import AllRentalOffersPage from '../all-rental-offers-page/all-rental-offers-page';
+
+type AppProps = {
+  stayPlacesCount: number
+};
+
+function App({stayPlacesCount}: AppProps): JSX.Element {
+  return (
+    <AllRentalOffersPage
+      stayPlacesCount={stayPlacesCount}
+    />
+  );
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Setting = {
+  STAY_PLACES_COUNT: 312,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App stayPlacesCount={Setting.STAY_PLACES_COUNT}/>
   </React.StrictMode>,
   document.getElementById('root'));
