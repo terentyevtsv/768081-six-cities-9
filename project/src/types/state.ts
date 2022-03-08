3185f11c89c3daf61a18802c4a58c609e8f5d62d
TerentevSV <terentyevtsv@ya.ru>
2022-03-08 19:27:50 +0400

    создал настроенный  экземпляр axios. Обновил код инициализации хранилища, чтобы dispatch научился принимать функции, а не только объекты

--- project/src/types/state.ts
+++ project/src/types/state.ts
@@ -1,7 +1,15 @@
 import { createStore } from 'redux';
 import { reducer } from '../store/reducer';
+import thunk from 'redux-thunk';
+import {applyMiddleware} from 'redux';
+import {createAPI} from '../services/api';
 
-export const store = createStore(reducer);
+export const api = createAPI();
+
+export const store = createStore(
+  reducer,
+  applyMiddleware(thunk),
+);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
