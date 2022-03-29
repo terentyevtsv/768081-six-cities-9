dee1c513d7aa42b8121dd92a955ac73ba27a30d6
TerentevSV <terentyevtsv@ya.ru>
2022-03-29 18:13:05 +0400

    написал тест роутинга

--- project/src/components/app/app.test.tsx
+++ project/src/components/app/app.test.tsx
@@ -2,25 +2,36 @@ import { configureMockStore } from '@jedmao/redux-mock-store';
 import { render, screen } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
 import { Provider } from 'react-redux';
-import { AppRoute, AuthorizationStatus, NameSpace, SortType } from '../../const';
+import { AppRoute, AuthorizationStatus, NameSpace, randomInteger, SortType } from '../../const';
 import { makeFakeCity, makeFakeOffers } from '../../utils/mock';
 import HistoryRouter from '../history-route/history-route';
 import App from './app';
+import thunk from 'redux-thunk';
 
-const mockStore = configureMockStore();
+const middlewares = [thunk];
+
+const mockStore = configureMockStore(middlewares);
 const history = createMemoryHistory();
 
 const fakeOffers = makeFakeOffers();
+const fakeOffer = fakeOffers[randomInteger(0, fakeOffers.length - 1)];
+
 const store = mockStore({
   [NameSpace.Rental]: {
     city: makeFakeCity(),
-    offers: fakeOffers,
+    offers: [],
     sortType: SortType.Popular,
   },
   [NameSpace.OffersData]: {
     areAllOffersLoaded: true,
     favoriteOffers: fakeOffers,
-    allOffers: fakeOffers,
+    allOffers: [],
+    currentOffer: fakeOffer,
+    nearOffers: [],
+    isOfferExist: true,
+  },
+  [NameSpace.ReviewsData]: {
+    offerReviews: [],
   },
   [NameSpace.User]: {
     authorizationStatus: AuthorizationStatus.Auth,
@@ -58,5 +69,25 @@ describe('App routing', () => {
     render(fakeApp);
 
     expect(screen.getByText(/Saved listing/i)).toBeInTheDocument();
+  });
+
+  it('should render RentalOfferPage when user navigate to /offer/:id', () => {
+    history.push(`/offer/${fakeOffer.id}`);
+
+    render(fakeApp);
+
+    expect(screen.getByText(/Meet the host/i)).toBeInTheDocument();
+  });
+
+  it('should render NotFound when user navigate fake App route', () => {
+    history.push('/fake');
+
+    render(fakeApp);
+
+    const headerElement = screen.getByText('Not Found');
+    const linkElement = screen.getByText('Go to main page');
+
+    expect(headerElement).toBeInTheDocument();
+    expect(linkElement).toBeInTheDocument();
   });
 });
